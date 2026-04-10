82248cb1702622d160f357a32c12ed060fb570b2
Abhinav Saxena <abhinavsaxena959@gmail.com>
2026-04-11 03:14:16 +0530

    Fix cors version and reinstall dependencies

--- backend/server.js
+++ backend/server.js
@@ -8,26 +8,21 @@ dotenv.config();
 
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
-// test route
 app.get("/", (req, res) => {
   res.send("API is running 🚀");
 });
 
-// routes
 app.use("/api/auth", authRoutes);
 
-// connect MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 
-// start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
